2f03fa73726a86862c27a059d96bcdf15f989d41
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-08-31 17:24:54 +0700

    update

--- Code.ptit/src/code/ptit/BoiSoChungUocSoChung.java
+++ Code.ptit/src/code/ptit/BoiSoChungUocSoChung.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package boisochung.uocsochung;
+package code.ptit;
 import java.util.*;
 /**
  *
