f6a691391259a085d1fca87d44f34ae60d510805
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-05 21:31:21 +0700

    WA

--- CodePtit/src/codeptit/TongUocSo.java
+++ CodePtit/src/codeptit/TongUocSo.java
@@ -1,37 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package codeptit;
-import java.util.*;
-/**
- *
- * @author suckm
- */
-public class TongUocSo {
+import java.util.Arrays;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class TongUocSo {
+    
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long temp,sum = 0;
-        long[] arr = new long[n];
-        for(int i=0;i<n;i++) {
-            temp = sc.nextLong();
-            for(long j = 2;j<=temp;j++){
-                while(temp % j == 0){
-                  System.out.print(temp + " ");
-                    sum += j;
-                    temp /= j;
+        int temp,sum = 0;
+        
+//        sang so nguyen to
+        int Prime_set[] = new int[200000];
+        boolean num[] = new boolean[2000000];
+        Arrays.fill(num,true);
+        num[0] = false;
+        num[1] = false;
+        int pos = 0;
+        for(int i=2;i*i<2000000;i++){
+            if(num[i]){
+                Prime_set[pos] = i;
+                pos++;
+                for(int j=i*i;j<2000000;j += i){
+                    num[j] = false;
                 }
             }
         }
-        System.out.println(sum);
+       
+        pos = 0;
+        for(int i=0; i<n;i++){
+            temp = sc.nextInt();
+            if( num[temp] ){
+                sum += temp;
+            }
+            else{
+                pos = 0;
+                while( temp != 1 ){
+                    while(temp % Prime_set[pos] == 0){
+                        sum += Prime_set[pos];
+                        temp /= Prime_set[pos];
+                    }
+                    if(num[temp]){
+                        sum += temp;
+                        break;
+                    }
+                    pos++;
+                }
+            }
+//            System.out.println(sum);
+        }
         
+        System.out.println(sum); 
     }
-    public static int isPrime
-    
 }
