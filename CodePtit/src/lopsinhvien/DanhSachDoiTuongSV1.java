66a508e18b3c14fc2a11120dbbfed6df32ccb335
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-10 08:40:28 +0700

    ac

--- CodePtit/src/lopsinhvien/DanhSachDoiTuongSV1.java
+++ CodePtit/src/lopsinhvien/DanhSachDoiTuongSV1.java
@@ -4,11 +4,35 @@
  * and open the template in the editor.
  */
 package lopsinhvien;
-
+import java.util.*;
+import lopsinhvien.SinhVien;
 /**
  *
  * @author suckm
  */
 public class DanhSachDoiTuongSV1 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        SinhVien sv = new SinhVien();
+//        sc.close();
+        int c = 1;
+        while(n-->0){
+            sc.reset();
+            sv.setStuID(c);
+            sv.setName(sc.nextLine());
+            sv.setLop(sc.next());
+            sv.setDob(sc.next());
+            sv.setGpa(sc.nextFloat());
+            sv.printstuID();
+            sv.printName();
+            sv.printLop();
+            sv.printDOB();
+            sv.printGPA();
+            System.out.println("");
+            sc.nextLine();
+            c++;
+            
+        }
+    }
 }
