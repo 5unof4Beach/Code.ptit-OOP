7df54e86ea68342dc7e5337f980bb597ed86ed3a
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-12 23:48:56 +0700

    ac

---
+++ CodePtit/src/tinhgiochoigame/Player.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tinhgiochoigame;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author suckm
+ */
+public class Player {
+    private String id,name,timeIn,timeOut;
+    private long timePlayed;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimeIn() {
+        return timeIn;
+    }
+
+    public void setTimeIn(String timeIn) {
+        this.timeIn = timeIn;
+    }
+
+    public String getTimeOut() {
+        return timeOut;
+    }
+
+    public void setTimeOut(String timeOut) {
+        this.timeOut = timeOut;
+    }
+    public void setTimePlay() throws ParseException{
+        SimpleDateFormat df = new SimpleDateFormat("hh:mm"); 
+        Date in = df.parse(timeIn);
+        Date out = df.parse(timeOut);
+        this.timePlayed = out.getTime() - in.getTime();
+//        
+    }
+
+    public long getTimePlayed(){
+        return timePlayed;
+    }
+    
+    
+    public void show() throws ParseException{
+        long temp = getTimePlayed();
+        long minTotal = temp / (60 * 1000);
+        long hour = minTotal/60;
+        long min = minTotal % 60;
+        System.out.printf("%s %s %d gio %d phut\n",id,name,hour,min);
+    }
+}
---
+++ CodePtit/src/tinhgiochoigame/TinhGio.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tinhgiochoigame;
+import java.text.ParseException;
+import java.util.*;
+
+public class TinhGio {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Vector<Player> res = new Vector<Player>();
+        for(int i=0;i<n;i++){
+            
+            Player player = new Player();
+            player.setId(sc.nextLine());
+            player.setName(sc.nextLine());
+            player.setTimeIn(sc.nextLine());
+            player.setTimeOut(sc.nextLine());
+            player.setTimePlay();
+//            sc.nextLine();
+            res.add(player);
+            
+        }
+        Collections.sort(res,new myCompare());
+        for(Player element:res){
+            element.show();
+        }
+    }
+}
+
+class myCompare implements Comparator<Player>{
+    public int compare(Player p1,Player p2){
+        if(p1.getTimePlayed() < p2.getTimePlayed())
+            return 1;
+        
+        else
+            return -1;
+        }
+    }
