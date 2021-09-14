66375152bc33db8afa87beb1b5809323d4bea1c0
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-14 19:49:28 +0700

    wa

---
+++ CodePtit/src/lopsinhvien/TreNhatGiaNhat.java
@@ -0,0 +1,21 @@
+
+package lopsinhvien;
+import java.text.ParseException;
+import java.util.*;
+
+public class TreNhatGiaNhat {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc= new Scanner(System.in);
+        Vector<SinhVien> res = new Vector<SinhVien>();
+        int n = Integer.parseInt(sc.nextLine());
+        for(int i=0;i<n;i++){
+            String name = sc.next();
+            String dob = sc.next();
+            SinhVien sv = new SinhVien(name,dob);
+            res.add(sv);
+        }
+        res.sort((i1,i2)->i2.getDob().compareTo(i1.getDob()));
+        System.out.println(res.elementAt(0).getName());
+        System.out.println(res.elementAt(res.size()-1).getName());
+    }
+}
