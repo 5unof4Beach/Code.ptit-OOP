6a7e3c6903e345ac1a09e1cefc747668304b78bf
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-15 15:25:28 +0700

    ac

---
+++ Một số bài quan trọng/XuLyVanBan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codeptit;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+/**
+ *
+ * @author suckm
+ */
+public class XuLyVanBan {
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("C:\\Users\\suckm\\OneDrive\\Desktop\\Test.txt");
+        Scanner sc = new Scanner(f);
+        String temp = "";
+        while(sc.hasNextLine()){
+            temp += sc.nextLine();
+        }
+        
+        temp = temp.replaceAll("\\s+", " ");
+        String res[] = temp.split("[.?!]");
+        for(String a:res){
+            a = a.trim();
+            a = a.toLowerCase();
+            a = a.substring(0,1).toUpperCase() + a.substring(1);
+            System.out.println(a);
+        }
+        
+    }
+}
