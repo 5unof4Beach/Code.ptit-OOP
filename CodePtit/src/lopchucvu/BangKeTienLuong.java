b1e99f25322e8cbf53b3c573915213c468644c6a
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-17 10:58:30 +0700

    J05038

---
+++ CodePtit/src/lopchucvu/BangKeTienLuong.java
@@ -0,0 +1,28 @@
+package lopchucvu;
+import java.util.*;
+/**
+ *
+ * @author suckm
+ */
+public class BangKeTienLuong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Vector<Employee> res = new Vector<Employee>();
+        for(int i=1;i<=n;i++){
+            String name = sc.nextLine();
+            long daySalary = sc.nextLong();
+            long worksDays = sc.nextLong();
+            sc.nextLine();
+            String pos = sc.nextLine();
+            Employee em = new Employee(i,name,daySalary,worksDays,pos);
+            res.add(em);
+        }
+        long total = 0;
+        for(Employee i:res){
+            i.show2();
+            total += i.getFinalPay();
+        }
+        System.out.println("Tong chi phi tien luong: "+total);
+    }
+}
