02b7b4c428ad5e018d4770f38463857a3a19b3ce
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-19 10:52:40 +0700

    ac

--- CodePtit/src/loppoint/Point.java
+++ CodePtit/src/loppoint/Point.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package loppoint;
 
-/**
- *
- * @author suckm
- */
 public class Point {
     private double x,y;
     
@@ -50,5 +41,4 @@ public class Point {
         double deltaY = Math.abs(p1.getY() - p2.getY());
         return(Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
     }
-//   
 }
