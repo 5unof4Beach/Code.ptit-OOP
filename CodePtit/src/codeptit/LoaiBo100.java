5343c6c3e7a3eec83593f6c7d102c0a9c65f808b
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-20 21:05:35 +0700

    ac

--- CodePtit/src/codeptit/LoaiBo100.java
+++ CodePtit/src/codeptit/LoaiBo100.java
@@ -1,20 +1,44 @@
 package codeptit;
 import java.util.*;
-
+//https://www.geeksforgeeks.org/length-longest-sub-string-can-make-removed/
 public class LoaiBo100 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n =Integer.parseInt(sc.nextLine());
         for(int i=0;i<n;i++){
             String num = sc.nextLine();
-            String temp = "";
-            int originalLength = num.length();
-            while(true){
-                temp = num;
-                num = num.replace("100", "");
-                if(temp.equals(num)) break;
-            }
-            System.out.printf("%d\n",originalLength - num.length());
+            count(num);
         }
+    }
+    public static void count(String s){
+        ArrayList<character> arr = new ArrayList<character>();
+        arr.add(new character('@',-1));
+        int max = 0;
+        for(int i=0;i<s.length();i++){
+            arr.add(new character(s.charAt(i),i));
+            
+            while(  arr.size() >= 3 && 
+                    arr.get(arr.size()-3).c == '1' &&
+                    arr.get(arr.size()-2).c == '0' &&
+                    arr.get(arr.size()-1).c == '0'){
+                
+                arr.remove(arr.size()-3);
+                arr.remove(arr.size()-2);
+                arr.remove(arr.size()-1);
+            }
+            int temp = arr.get(arr.size()-1).index;
+            max = Math.max(max, i - temp);
+               
+        }
+        System.out.println(max);
+    }
+}
+
+class character{
+    char c;
+    int index;
+    public character(char c,int i){
+        this.c = c;
+        this.index = i;
     }
 }
