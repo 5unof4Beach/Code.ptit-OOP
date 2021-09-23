6a21d49c4efab4ca1bf21ac80caa5042c3f33e60
Bùi Minh Đức <suckmydick2001gta@gmail.com>
2021-09-23 19:18:53 +0700

    ac

--- CodePtit/src/Test.java
+++ CodePtit/src/Test.java
@@ -1,18 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.*;
 
-/**
- *
- * @author suckm
- */
 public class Test {
     public static void main(String[] args) {
-        double rate = (24100)*2/3;
-        System.out.println((double) rate );
-        rate = rate/1000;
-        System.out.println( Math.round(rate) );
+        Scanner sc = new Scanner(System.in);
+        HashMap<Long,Long> heSoVaBac = new HashMap<Long,Long>();
+        String s = sc.nextLine();
+        StringTokenizer res = new StringTokenizer(s," + ");
+        while(res.hasMoreTokens()){
+            String temp = res.nextToken().toString();
+            System.out.println(temp);
+            String bac = temp.substring(temp.indexOf('^')+1);
+            String heSo = temp.substring(0,temp.indexOf('*'));
+            heSoVaBac.put(convert(bac),convert(heSo));
+            for (Iterator<Long> it = heSoVaBac.keySet().iterator(); it.hasNext();) {
+                System.out.println(it.next().longValue());
+            }
+        }
+    }
+    
+    public static long convert(String s){
+        long res = 0;
+        for(int i=0;i<s.length();i++){
+            res = res*10 + (s.charAt(i) - '0');
+        }
+        return res;
     }
 }
---
+++ CodePtit/src/TongDaThuc/Main.java
@@ -0,0 +1,15 @@
+package TongDaThuc;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while(t-->0){
+            DaThuc p = new DaThuc(sc.nextLine());
+            DaThuc q = new DaThuc(sc.nextLine());
+            DaThuc r = p.cong(q);
+            System.out.println(r);
+        }
+    }
+}
